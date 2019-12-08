237f4e3c09956ee7c41560a6165af71c943360a6
lawrencemutex <57431983+lawrencemutex@users.noreply.github.com>
2019-12-08 23:28:30 +0300

    /build
    build folder included

---
+++ build/precache-manifest.d6ad13f39838c2409748003ea9a1a04e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0d4aefa040d3cf4eb40b",
+    "url": "/static/js/main.0d4aefa0.chunk.js"
+  },
+  {
+    "revision": "310b19b5b74b41284534",
+    "url": "/static/js/2.310b19b5.chunk.js"
+  },
+  {
+    "revision": "0d4aefa040d3cf4eb40b",
+    "url": "/static/css/main.15249efd.chunk.css"
+  },
+  {
+    "revision": "310b19b5b74b41284534",
+    "url": "/static/css/2.302028f1.chunk.css"
+  },
+  {
+    "revision": "79cc2f5e5c0a339f79eb6c61aba968b9",
+    "url": "/index.html"
+  }
+];
